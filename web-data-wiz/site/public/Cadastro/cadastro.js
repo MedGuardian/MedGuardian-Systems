d9c29d7434e52a74eea47d7ec2ee5d3aaf64d8be
Uglúk <125396897+PedroHSilva1009@users.noreply.github.com>
2023-09-06 22:56:12 -0300

    Mascara de cnpj e validados de cnpj valido

--- web-data-wiz/site/public/Cadastro/cadastro.js
+++ web-data-wiz/site/public/Cadastro/cadastro.js
@@ -52,3 +52,61 @@ function cadastrar() {
     }
 
 }
+
+function transformarCnpj(){
+    var cnpj = inputCNPJ.value.replace(/[^\d]/g, '');
+
+            let cnpjFormatado = '';
+
+                cnpjFormatado = `${cnpj.substr(0, 2)}.${cnpj.substr(2, 3)}.${cnpj.substr(5, 3)}/${cnpj.substr(8, 4)}-${cnpj.substr(12)}`;
+
+            inputCNPJ.value = cnpjFormatado;
+}
+
+function validaCNPJ(){
+    cnpj=inputCNPJ.value
+    numerosVerificacao = cnpj.replaceAll(/\D/g,"");
+    
+    if(numerosVerificacao.length != 14){
+        inputCNPJ.style.borderColor = 'red';
+        return false;
+    }else{
+        var digitosVerificadores = numerosVerificacao[numerosVerificacao.length-2] + numerosVerificacao[numerosVerificacao.length-1];
+        var numerosVerificacao = numerosVerificacao.slice(0, numerosVerificacao.length-2);
+        var digitos = gerarDigitos(numerosVerificacao);
+        if(digitos == digitosVerificadores){
+            inputCNPJ.style.color = 'black';
+            return true;
+        }else{
+            inputCNPJ.style.color = 'red';
+            return false;
+        }
+    }
+}
+
+function gerarDigitos(numerosVerificacao){
+    var soma = 0;
+    var peso = 2;
+    for(var i = numerosVerificacao.length-1; i >= 0; i--){
+        soma += numerosVerificacao[i] * peso;
+
+        if(peso == 9){
+            peso = 2;
+        }else{
+            peso++;
+        }
+    }
+    
+    var modulo = soma % 11;
+    var digitoVerificador = 0;
+
+    if(modulo >= 2){
+        digitoVerificador = 11 - modulo;
+    }
+
+    if(numerosVerificacao.length < 13){
+        digitoVerificador += `${gerarDigitos(numerosVerificacao + digitoVerificador)}`;
+    }
+
+    return digitoVerificador;
+}
