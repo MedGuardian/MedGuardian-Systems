bfbc8bd87ef400057df5e03f970b4cf0595ad2cd
lucasfari-a <lucas.flima@sptech.school>
2023-09-12 13:27:36 -0300

    @lucasfari-a Landing Page Responsivo

--- web-data-wiz/site/public/PaginaInicial/paginaInicial.js
+++ web-data-wiz/site/public/PaginaInicial/paginaInicial.js
@@ -23,9 +23,9 @@
       spanConteudoMeioDireitaSobreNos.innerHTML = 'Dados precisos e atualizados em tempo real, os erros podem ser minimizados, aumentando a segurança do paciente.';
       spanConteudoInferiorDireitaSobreNos.innerHTML = 'Pacientes podem acessar seus próprios registros médicos de forma segura.';
 
-      spanTextoMissao.innerHTMTL = 'Capacitar e monitorar os sistema e garantir a segurança e eficiência e de seus dados por meio da monitoração avançada de computadores.';
-      spanTextoVisao.innerHTMTL = 'Ser reconhecidos como o parceiros de confiança quando se trata de gerenciar o desempenho de computadores e coleta de dados.';
-      spanTextoValores.innerHTMTL = 'Inovação. Excelência. Colaboração. Entender necessidades. Responsabilidade. Sustentáveis,';
+      spanTextoMissao.innerHTML = 'Capacitar e monitorar os sistema e garantir a segurança e eficiência e de seus dados por meio da monitoração avançada de computadores.';
+      spanTextoVisao.innerHTML = 'Ser reconhecidos como o parceiros de confiança quando se trata de gerenciar o desempenho de computadores e coleta de dados.';
+      spanTextoValores.innerHTML = 'Inovação. Excelência. Colaboração. Entender necessidades. Responsabilidade. Sustentabilidade.';
     } else {
       spanConteudoSuperiorEsquerdaSobreNos.innerHTML = 'Ao ter acesso rápido a informações relevantes sobre os pacientes, os profissionais de saúde podem tomar decisões mais informadas, proporcionando um atendimento mais preciso e personalizado.';
       spanConteudoMeioEsquerdaSobreNos.innerHTML = 'Ao centralizar os dados de pacientes, será possível realizar análises de tendências de saúde, identificar surtos de doenças e tomar medidas preventivas em nível populacional.';
@@ -34,9 +34,9 @@
       spanConteudoMeioDireitaSobreNos.innerHTML = 'O sistema permitirá que os médicos, enfermeiros e outros profissionais de saúdecompartilhem informações de forma rápida e eficaz, melhorando a colaboração e a coordenação no cuidado do paciente.';
       spanConteudoInferiorDireitaSobreNos.innerHTML = 'O sistema permitirá que pacientes acessem seus próprios registros médicos de forma segura, incentivando a participação ativa na gestão de sua saúde.';
 
-      spanTextoMissao.innerHTMTL = 'Nossa missão é capacitar e monitorar os sistemas para garantir a segurança, a eficiência e a integridade de seus dados por meio da monitoração avançada de computadores. Através do nosso sistema de monitoramento de coleta e armazenamento de dados, estamos comprometidos em oferecer soluções de monitoramento inteligentes, permitindo que nossos clientes tomem decisões informadas, protejam suas informações sensíveis e otimizem o desempenho de suas operações.'
-      spanTextoVisao.innerHTMTL = 'Nossa visão é liderar a indústria de monitoramento de computadores, sendo reconhecidos como o parceiro de confiança quando se trata de gerenciar o desempenho de computadores que coletam dado. Planejamos alcançar isso expandindo nossa presença global, desenvolvendo constantemente tecnologias inovadoras e oferecendo um serviço excepcional.'
-      spanTextoValores.innerHTMTL = ' Estamos comprometidos com a constante busca por inovação e excelência. Acreditamos no poder da colaboração. Trabalhamos em estreita parceria com nossos clientes para entender suas necessidades.Reconhecemos nossa responsabilidade em contribuir positivamente para a sociedade. Promovemos práticas sustentáveis, respeitamos normas legais e éticas e buscamos oportunidades de impacto social positivo através de nossa tecnologia.'
+      spanTextoMissao.innerHTML = 'Nossa missão é capacitar e monitorar os sistemas para garantir a segurança, a eficiência e a integridade de seus dados por meio da monitoração avançada de computadores. Através do nosso sistema de monitoramento de coleta e armazenamento de dados, estamos comprometidos em oferecer soluções de monitoramento inteligentes, permitindo que nossos clientes tomem decisões informadas, protejam suas informações sensíveis e otimizem o desempenho de suas operações.'
+      spanTextoVisao.innerHTML = 'Nossa visão é liderar a indústria de monitoramento de computadores, sendo reconhecidos como o parceiro de confiança quando se trata de gerenciar o desempenho de computadores que coletam dado. Planejamos alcançar isso expandindo nossa presença global, desenvolvendo constantemente tecnologias inovadoras e oferecendo um serviço excepcional.'
+      spanTextoValores.innerHTML = ' Estamos comprometidos com a constante busca por inovação e excelência. Acreditamos no poder da colaboração. Trabalhamos em estreita parceria com nossos clientes para entender suas necessidades.Reconhecemos nossa responsabilidade em contribuir positivamente para a sociedade. Promovemos práticas sustentáveis, respeitamos normas legais e éticas e buscamos oportunidades de impacto social positivo através de nossa tecnologia.'
     }
   }
 
