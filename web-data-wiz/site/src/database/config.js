b6f68d1123e55e752b688b1508b13461d0e0347d
Uglúk <125396897+PedroHSilva1009@users.noreply.github.com>
2023-10-20 13:53:59 -0300

    Update config.js

--- web-data-wiz/site/src/database/config.js
+++ web-data-wiz/site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "medGuardian",
-    user: "root",
-    password: "@21102002Lu",
+    user: "aluno",
+    password: "aluno",
 };
 
 function executar(instrucao) {
