baadd5faa3585baf6ca1498a53d26b8739ccd5fb
Uglúk <125396897+PedroHSilva1009@users.noreply.github.com>
2023-10-20 13:59:53 -0300

    Update usuarioModel.js

--- web-data-wiz/site/src/models/usuarioModel.js
+++ web-data-wiz/site/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(cnpj, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ")
     var instrucao = `
-        SELECT * FROM Empresa WHERE cnpjEmpresa = '${cnpj}' AND senhaEmpresa = '${senha}';
+        SELECT * FROM empresa WHERE cnpjEmpresa = '${cnpj}' AND senhaEmpresa = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -15,7 +15,7 @@ function cadastrar(razaoSocial, cnpj, email, contato, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO Empresa (razaoSocial, cnpjEmpresa, emailEmpresa, contatoEmpresa, senhaEmpresa) 
+        INSERT INTO empresa (razaoSocial, cnpjEmpresa, emailEmpresa, contatoEmpresa, senhaEmpresa) 
         VALUES ('${razaoSocial}', '${cnpj}', '${email}', '${contato}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
@@ -40,7 +40,7 @@ function cadastrarEnd(cep, log, num, comp) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO Endereco (cep, logradouro, numeroEmpresa, complementoEmpresa, fkEmpresa) VALUES
+        INSERT INTO endereco (cep, logradouro, numeroEmpresa, complementoEmpresa, fkEmpresa) VALUES
         ('${cep}', '${log}', '${num}', '${comp}', (SELECT MAX(idEmpresa)FROM empresa));
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
