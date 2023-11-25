c39915a7c8a3f148a4ea51148619ae244a1309db
KatherinneAssis <Katherinne.silva@sptech.school>
2023-11-25 11:42:46 -0300

    @katherinneassis Alterações no JS da dashEspecifica

--- web-data-wiz/site/src/database/config.js
+++ web-data-wiz/site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "medguardian",
     user: "root",
-    password: "@21102002Lu",
+    password: "272505",
 };
 
 function executar(instrucao) {
