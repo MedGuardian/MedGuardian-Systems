9e06d0ef1ea7b8120be771c03e66b4ab33635ed3
Elera <victor.cardoso@sptech.school>
2023-11-27 19:35:19 -0300

    Script finalizado 2

--- instalacao.sh
+++ instalacao.sh
@@ -46,35 +46,36 @@ cat<<EOL >> tabelas.sql
  USE medguardian ;
 
 CREATE TABLE IF NOT EXISTS computador( 
- idComputador INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
- nomeComputador VARCHAR(255) NOT NULL, 
- sistemaOperacional VARCHAR(255) NOT NULL 
- ); 
+ 	idComputador INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+ 	nomeComputador VARCHAR(255) NOT NULL, 
+ 	sistemaOperacional VARCHAR(255) NOT NULL 
+); 
 
- CREATE TABLE IF NOT EXISTS componente( 
- idComponente INT AUTO_INCREMENT NOT NULL, 
- tabelas.sql >>	nomeComponente VARCHAR(225) NOT NULL, 
- PRIMARY KEY (idComponente)); 
+CREATE TABLE IF NOT EXISTS componente( 
+ 	idComponente INT AUTO_INCREMENT NOT NULL, 
+ 	tabelas.sql >>	nomeComponente VARCHAR(225) NOT NULL, 
+ 	PRIMARY KEY (idComponente)
+); 
 
- CREATE TABLE IF NOT EXISTS especificacao( 
- idEspecificacao INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
- fkComputador INT NOT NULL,
- fkComponente INT NOT NULL, 
- totalComponente DECIMAL(6,2) NULL, 
- CONSTRAINT fk_computador_especificacao FOREIGN KEY (fkComputador) 
- REFERENCES computador(idComputador), 
- CONSTRAINT fk_componente_especificacao FOREIGN KEY (fkComponente) 
- tabelas.sql >>	REFERENCES componente(idComponente) 
- ); 
+CREATE TABLE IF NOT EXISTS especificacao( 
+ 	idEspecificacao INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+ 	fkComputador INT NOT NULL,
+ 	fkComponente INT NOT NULL, 
+ 	totalComponente DECIMAL(6,2) NULL, 
+ 	CONSTRAINT fk_computador_especificacao FOREIGN KEY (fkComputador) 
+ 	REFERENCES computador(idComputador), 
+ 	CONSTRAINT fk_componente_especificacao FOREIGN KEY (fkComponente) 
+	tabelas.sql >>	REFERENCES componente(idComponente) 
+); 
 
- CREATE TABLE IF NOT EXISTS registro( 
- idregistro INT AUTO_INCREMENT NOT NULL, 
- dataHoraRegistro DATETIME NOT NULL, 
- registro DECIMAL(6,2) NOT NULL, 
- tabelas.sql >>	tipoCaptura VARCHAR(255) NULL, 
- tabelas.sql >>	fkEspecificacao INT NOT NULL, 
- PRIMARY KEY (idregistro) 
- ); ;
+CREATE TABLE IF NOT EXISTS registro( 
+ 	idregistro INT AUTO_INCREMENT NOT NULL, 
+ 	dataHoraRegistro DATETIME NOT NULL, 
+ 	registro DECIMAL(6,2) NOT NULL, 
+ 	tabelas.sql >>	tipoCaptura VARCHAR(255) NULL, 
+ 	tabelas.sql >>	fkEspecificacao INT NOT NULL, 
+ 	PRIMARY KEY (idregistro) 
+); 
 EOL
 cd ..
 touch Dockerfile
