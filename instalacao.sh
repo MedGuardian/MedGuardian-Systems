34ad77baf462abe86c6bdfde00f122abc9fcd285
Elera <victor.cardoso@sptech.school>
2023-11-27 19:08:01 -0300

    Script

--- instalacao.sh
+++ instalacao.sh
@@ -54,39 +54,39 @@ CREATE TABLE IF NOT EXISTS computador(
  idComputador INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
  nomeComputador VARCHAR(255) NOT NULL, 
  sistemaOperacional VARCHAR(255) NOT NULL 
- ); ;
+ ); 
 
- CREATE TABLE IF NOT EXISTS componente( ;
- idComponente INT AUTO_INCREMENT NOT NULL, ;
- tabelas.sql >>	nomeComponente VARCHAR(225) NOT NULL, ;
- PRIMARY KEY (idComponente)); ;
+ CREATE TABLE IF NOT EXISTS componente( 
+ idComponente INT AUTO_INCREMENT NOT NULL, 
+ tabelas.sql >>	nomeComponente VARCHAR(225) NOT NULL, 
+ PRIMARY KEY (idComponente)); 
 
- CREATE TABLE IF NOT EXISTS especificacao( ;
- idEspecificacao INT AUTO_INCREMENT PRIMARY KEY NOT NULL, ;
- fkComputador INT NOT NULL, ;
- fkComponente INT NOT NULL, ;
- totalComponente DECIMAL(6,2) NULL, ;
- CONSTRAINT fk_computador_especificacao FOREIGN KEY (fkComputador) ;
- REFERENCES computador(idComputador), ;
- CONSTRAINT fk_componente_especificacao FOREIGN KEY (fkComponente) ;
- tabelas.sql >>	REFERENCES componente(idComponente) ;
- ); ;
+ CREATE TABLE IF NOT EXISTS especificacao( 
+ idEspecificacao INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+ fkComputador INT NOT NULL,
+ fkComponente INT NOT NULL, 
+ totalComponente DECIMAL(6,2) NULL, 
+ CONSTRAINT fk_computador_especificacao FOREIGN KEY (fkComputador) 
+ REFERENCES computador(idComputador), 
+ CONSTRAINT fk_componente_especificacao FOREIGN KEY (fkComponente) 
+ tabelas.sql >>	REFERENCES componente(idComponente) 
+ ); 
 
- CREATE TABLE IF NOT EXISTS registro( ;
- idregistro INT AUTO_INCREMENT NOT NULL, ;
- dataHoraRegistro DATETIME NOT NULL, ;
- registro DECIMAL(6,2) NOT NULL, ;
- tabelas.sql >>	tipoCaptura VARCHAR(255) NULL, ;
- tabelas.sql >>	fkEspecificacao INT NOT NULL, ;
- PRIMARY KEY (idregistro) ;
+ CREATE TABLE IF NOT EXISTS registro( 
+ idregistro INT AUTO_INCREMENT NOT NULL, 
+ dataHoraRegistro DATETIME NOT NULL, 
+ registro DECIMAL(6,2) NOT NULL, 
+ tabelas.sql >>	tipoCaptura VARCHAR(255) NULL, 
+ tabelas.sql >>	fkEspecificacao INT NOT NULL, 
+ PRIMARY KEY (idregistro) 
  ); ;
 EOL
 cd ..
 touch Dockerfile
 echo FROM mysql:latest >> Dockerfile;
-echo ENV MYSQL_ROOT_PASSWORD=root >> Dockerfile
-echo COPY ./Dock/ /docker-entrypoint-initdb.d/ >> Dockerfile
-echo  EXPOSE 3306 >> Dockerfile
+echo ENV MYSQL_ROOT_PASSWORD=root >> Dockerfile;
+echo COPY ./Dock/ /docker-entrypoint-initdb.d/ >> Dockerfile;
+echo EXPOSE 3306 >> Dockerfile;
 sudo docker build -t meu-banco .
 
 sudo docker run -d --name meu-container -p 3306:3306 meu-banco
