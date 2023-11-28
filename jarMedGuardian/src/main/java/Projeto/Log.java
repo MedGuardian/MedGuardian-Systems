abda322ece04372de877d516b78d6fcd2db9d978
lucasfari-a <lucas.flima@sptech.school>
2023-11-27 21:04:39 -0300

    @lucasfari-a

--- jarMedGuardian/src/main/java/Projeto/Log.java
+++ jarMedGuardian/src/main/java/Projeto/Log.java
@@ -32,7 +32,7 @@ public class Log {
         String dataHoraAtual = dateFormat.format(new Date());
 
         String nomeDoArquivo = dataHoraAtual;
-        String caminhoDoArquivo = "C:\\Users\\sarah\\OneDrive\\Área de Trabalho\\Logs\\" + nomeDoArquivo + ".txt";
+        String caminhoDoArquivo = "C:\\Users\\lucas\\OneDrive\\Área de Trabalho\\Logs\\" + nomeDoArquivo + ".txt";
 
         try {
             FileWriter arq = new FileWriter(caminhoDoArquivo);
@@ -77,7 +77,7 @@ public class Log {
             String dataHoraAtual = dateFormat.format(new Date());
 
             String nomeDoArquivo = "Erros_" + dataHoraAtual;
-            String caminhoDoArquivo = "C:\\Users\\sarah\\OneDrive\\Área de Trabalho\\Logs\\" + nomeDoArquivo + ".txt";
+            String caminhoDoArquivo = "C:\\Users\\lucas\\OneDrive\\Área de Trabalho\\Logs\\x1" + nomeDoArquivo + ".txt";
 
             FileWriter arq = new FileWriter(caminhoDoArquivo);
             PrintWriter gravarArq = new PrintWriter(arq);
