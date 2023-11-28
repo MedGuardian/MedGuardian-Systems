f41b51723a9e5f858c7888accfe89dd07903ce1a
Elera <victor.cardoso@sptech.school>
2023-11-28 14:59:10 -0300

    Script

--- instalacao.sh
+++ instalacao.sh
@@ -39,53 +39,16 @@ fi
 mkdir Dock
 cd Dock
 
-touch tabelas.sql
-cat<<EOL >> tabelas.sql
- DROP DATABASE IF EXISTS medguardian ;
- CREATE DATABASE medguardian ;
- USE medguardian ;
+wget https://github.com/MedGuardian/MedGuardian-Systems/raw/Beralde-Individual/scriptsql.sql
 
-CREATE TABLE IF NOT EXISTS computador( 
- 	idComputador INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
- 	nomeComputador VARCHAR(255) NOT NULL, 
- 	sistemaOperacional VARCHAR(255) NOT NULL 
-); 
-
-CREATE TABLE IF NOT EXISTS componente( 
- 	idComponente INT AUTO_INCREMENT NOT NULL, 
- 	nomeComponente VARCHAR(225) NOT NULL, 
- 	PRIMARY KEY (idComponente)
-);
-
-CREATE TABLE IF NOT EXISTS especificacao( 
- 	idEspecificacao INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
- 	fkComputador INT NOT NULL,
- 	fkComponente INT NOT NULL, 
- 	totalComponente DECIMAL(6,2) NULL, 
- 	CONSTRAINT fk_computador_especificacao FOREIGN KEY (fkComputador) 
- 	REFERENCES computador(idComputador), 
- 	CONSTRAINT fk_componente_especificacao FOREIGN KEY (fkComponente) 
-	REFERENCES componente(idComponente) 
-); 
-
-CREATE TABLE IF NOT EXISTS registro( 
- 	idregistro INT AUTO_INCREMENT NOT NULL, 
- 	dataHoraRegistro DATETIME NOT NULL, 
- 	registro DECIMAL(6,2) NOT NULL, 
-	tipoCaptura VARCHAR(255) NULL, 
-	fkEspecificacao INT NOT NULL, 
- 	PRIMARY KEY (idregistro) 
-); 
-EOL
 cd ..
 
 cat<<EOL >> Dockerfile
- FROM mysql:latest 
- ENV MYSQL_ROOT_PASSWORD=root 
- COPY ./Dock/ /docker-entrypoint-initdb.d/ 
- EXPOSE 3306 /
+FROM mysql:latest 
+ENV MYSQL_ROOT_PASSWORD=root 
+COPY ./Dock/ /docker-entrypoint-initdb.d/ 
+EXPOSE 3306
 EOL
 
 sudo docker build -t meu-banco .
 sudo docker run -d --name meu-container -p 3306:3306 meu-banco
-sudo docker start meu-container
