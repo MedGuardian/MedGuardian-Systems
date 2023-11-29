46f30aee60ed3b8d030c9ce409abc22f5a2f4c43
SarahVS02 <sarah.silva@sptech.school>
2023-11-28 22:01:35 -0300

    Atualização Jar

--- jarMedGuardian/src/main/java/Projeto/TesteProjetoSarah.java
+++ jarMedGuardian/src/main/java/Projeto/TesteProjetoSarah.java
@@ -2,7 +2,6 @@ package Projeto;
 
 import com.github.britooo.looca.api.core.Looca;
 import java.io.IOException;
-
 import java.util.*;
 
 public class TesteProjetoSarah {
@@ -14,20 +13,17 @@ public class TesteProjetoSarah {
         Componente HD = new Componente(looca.getGrupoDeDiscos().getDiscos().get(0).getModelo());
         Componente RAM = new Componente("RAM");
         Componente PROCESSADOR = new Componente(looca.getProcessador().getNome());
-        Componente REDE = new Componente("REDE");
         Componente TEMPOATIVIDADE = new Componente("TEMPO DE ATIVIDADE");
 
         String nomeComputador = looca.getRede().getParametros().getNomeDeDominio();
         Integer conversorGb = 1000000000;
-        Integer conversorMb = 1000000;
         Boolean logado = false;
-        Integer idComputador;
+        Integer idComputador = null;
         Integer idFuncionario = null;
-        Integer hd = 0;
-        int pid;
         String sistemaOperacional;
 
         do {
+            ObterMemoriaSwap.ObterMemoriaSwap();
             System.out.println("Digite o email: ");
             Scanner leitorEmail = new Scanner(System.in);
             String email = leitorEmail.nextLine();
@@ -38,63 +34,82 @@ public class TesteProjetoSarah {
             if(!bancoDeDados.autenticarUsuario(email, senha).isEmpty()){
                 logado = true;
                 idFuncionario = bancoDeDados.autenticarUsuario(email, senha).get(0).getIdFuncionario();
+
+                if(System.getProperty("os.name").toLowerCase().contains("win")){
+                    sistemaOperacional = "Windows";
+                } else {
+                    sistemaOperacional = "Linux";
+                }
+
+                if(bancoDeDados.verificarComputadorCadastrado(nomeComputador)){
+                    Integer fkEmpresaDoFuncionario = bancoDeDados.getFkEmpresaPorIdFuncionario(idFuncionario);
+                    bancoDeDados.insertComputador(nomeComputador, fkEmpresaDoFuncionario, sistemaOperacional);
+                    idComputador = bancoDeDados.selectIdComputador(nomeComputador);
+
+                    bancoDeDados.insertComponente(PROCESSADOR.getNomeComponente());
+                    bancoDeDados.insertComponente(RAM.getNomeComponente());
+                    if(!looca.getGrupoDeDiscos().getVolumes().isEmpty()){
+                        for(int i = 0; i < looca.getGrupoDeDiscos().getQuantidadeDeDiscos(); i++) {
+                            bancoDeDados.insertComponente(HD.getNomeComponente() + (i + 1));
+                        }
+                    }
+                    bancoDeDados.insertComponente(TEMPOATIVIDADE.getNomeComponente());
+
+                    for(int i = 0; i < bancoDeDados.selectComponente().size(); i++){
+                        Integer idComponente = bancoDeDados.selectComponente().get(i).getIdComponente();
+
+                        switch (idComponente) {
+                            case 1 -> {
+                                bancoDeDados.insertEspecificacao(idComputador, idComponente, 100.);
+
+                            }
+                            case 2 -> {
+                                bancoDeDados.insertEspecificacao(idComputador, idComponente, looca.getMemoria().getTotal().doubleValue() / conversorGb);
+                            }
+                            case 3 ->
+                                    bancoDeDados.insertEspecificacao(idComputador, idComponente, ((looca.getGrupoDeDiscos().getVolumes().get(0).getTotal().doubleValue() / conversorGb)) - 30);
+                            case 4 ->
+                                    bancoDeDados.insertEspecificacao(idComputador, idComponente, null);
+                        }
+                    }
+                } else {
+                    idComputador = bancoDeDados.selectIdComputador(nomeComputador);
+                }
+
+                if(bancoDeDados.getFkEmpresaPorIdFuncionario(idFuncionario) != bancoDeDados.getFkEmpresaDaMaquinaPeloNome(nomeComputador)){
+                    logado = false;
+                    System.out.println("Você não é um funcionário registrado na empresa linkada a essa máquina!");
+                    System.out.println("Solicite para que alguém libere seu acesso, se for o caso.");
+                    Log log = new Log();
+                    log.gravarLogErros();
+
+                } else {
+                    System.out.println("""
+                USUÁRIO %s AUTENTICADO COM SUCESSO!
+                INICIANDO A CAPTURA DE DADOS DA MÁQUINA...
+                """.formatted(bancoDeDados.autenticarUsuario(email, senha).get(0).getNomeFuncionario()));
+
+
+
+                }
             }
         } while (!logado);
 
-        if(bancoDeDados.verificarComputadorCadastrado(nomeComputador)){
-            if(System.getProperty("os.name").toLowerCase().contains("win")){
-                sistemaOperacional = "Windows";
-            } else {
-                sistemaOperacional = "Linux";
-            }
-            bancoDeDados.insertComputador(nomeComputador, bancoDeDados.getFkEmpresaPorIdFuncionario(idFuncionario), sistemaOperacional);
-            idComputador = bancoDeDados.selectIdComputador(nomeComputador);
-            if(!looca.getGrupoDeDiscos().getVolumes().isEmpty()){
-                for(int i = 0; i < looca.getGrupoDeDiscos().getQuantidadeDeDiscos(); i++) {
-                    bancoDeDados.insertComponente(HD.getNomeComponente() + (i + 1));
-                    hd++;
-                }
-            }
-            bancoDeDados.insertComponente(RAM.getNomeComponente());
-            bancoDeDados.insertComponente(PROCESSADOR.getNomeComponente());
-            bancoDeDados.insertComponente(REDE.getNomeComponente());
-            bancoDeDados.insertComponente(TEMPOATIVIDADE.getNomeComponente());
-
-            for(int i = 0; i < bancoDeDados.selectComponente().size(); i++){
-                String nomeComponente = bancoDeDados.selectComponente().get(i).getNomeComponente();
-                Integer idComponente = bancoDeDados.selectComponente().get(i).getIdComponente();
-
-                switch (nomeComponente) {
-                    case "HD1" -> {
-                        bancoDeDados.insertEspecificacao(idComputador, idComponente, looca.getGrupoDeDiscos().getDiscos().get(0).getTamanho().doubleValue() / conversorGb);
-                    }
-                    case "RAM" -> {
-                        bancoDeDados.insertEspecificacao(idComputador, idComponente, looca.getMemoria().getTotal().doubleValue() / conversorGb);
-                    }
-                    case "PROCESSADOR" ->
-                            bancoDeDados.insertEspecificacao(idComputador, idComponente, 100.);
-                    case "REDE", "TEMPO DE ATIVIDADE" ->
-                            bancoDeDados.insertEspecificacao(idComputador, idComponente, null);
-                }
-            }
-        } else {
-            idComputador = bancoDeDados.selectIdComputador(nomeComputador);
-        }
-
         Timer timer = new Timer();
         int delay = 0; // Tempo de espera antes da primeira execução (0 significa que será executado imediatamente)
-        int interval = 3000; // Intervalo entre as execuções em milissegundos (5 segundos)
+        int interval = 5000; // Intervalo entre as execuções em milissegundos (5 segundos)
 
         Integer finalIdComputador = idComputador;
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
                 System.out.println("DADOS SENDO MONITORADOS...");
-                Double discoEmUso = looca.getGrupoDeDiscos().getVolumes().get(0).getDisponivel().doubleValue() / conversorGb;
+
+                Double discoDisponivel = looca.getGrupoDeDiscos().getVolumes().get(0).getDisponivel().doubleValue() / conversorGb;
                 Double memoriaRamEmUso = looca.getMemoria().getEmUso().doubleValue() / conversorGb;
                 Double processadorEmUso = looca.getProcessador().getUso();
+                Double swapDisponivel = ObterMemoriaSwap.ObterMemoriaSwap().get(0).doubleValue() / conversorGb;
                 Double numeroThreads = looca.getGrupoDeProcessos().getTotalThreads().doubleValue();
                 Double numeroProcessos = looca.getGrupoDeProcessos().getTotalProcessos().doubleValue();
-                Double redeAtual;
                 Integer segundos = looca.getSistema().getTempoDeAtividade().intValue();
 
                 Integer dias = segundos / 86400;
@@ -106,41 +121,33 @@ public class TesteProjetoSarah {
                 Integer minutos = segundos / 60;
                 segundos = segundos % 60;
 
-                if (dias >= 10) {
-                    Log log = new Log();
-                    log.gravarLogErros();
-                }
 
-                if((looca.getRede().getGrupoDeInterfaces().getInterfaces().get(4).getBytesEnviados().doubleValue() / conversorMb) > 0){
-                    redeAtual = (looca.getRede().getGrupoDeInterfaces().getInterfaces().get(4).getPacotesEnviados().doubleValue() + looca.getRede().getGrupoDeInterfaces().getInterfaces().get(4).getPacotesRecebidos().doubleValue()) / conversorMb;
-                } else {
-                    redeAtual = (looca.getRede().getGrupoDeInterfaces().getInterfaces().get(3).getPacotesEnviados().doubleValue() + looca.getRede().getGrupoDeInterfaces().getInterfaces().get(3).getPacotesRecebidos().doubleValue()) / conversorMb;
-                }
 
                 for(int i = 0; i < bancoDeDados.selectComponente().size(); i++){
                     Integer idComponente = bancoDeDados.selectComponente().get(i).getIdComponente();
-                    String nomeComponente = bancoDeDados.selectComponente().get(i).getNomeComponente();
 
-                    switch (nomeComponente) {
-                        case "HD1" -> {
-                            bancoDeDados.insertRegistro(discoEmUso, "Uso", 1);
+                    switch (idComponente) {
+                        case  1-> {
+                            bancoDeDados.insertRegistro(processadorEmUso, "UsoCpu", 1);
+                            bancoDeDados.insertRegistro(Double.valueOf(dias), "Dias", 1);
+                            bancoDeDados.insertRegistro(Double.valueOf(horas), "Horas", 1);
+                            bancoDeDados.insertRegistro(Double.valueOf(minutos), "Minutos", 1);
+                            bancoDeDados.insertRegistro(Double.valueOf(segundos), "Segundos", 1);
+                            bancoDeDados.insertRegistro(numeroProcessos, "QuantidadeProcessos", 1);
+                            bancoDeDados.insertRegistro(numeroThreads, "QuantidadeThreads", 1);
                         }
-                        case "RAM" -> {
+                        case 2 -> {
                             bancoDeDados.insertRegistro(memoriaRamEmUso, "Uso", 2);
                         }
-                        case "PROCESSADOR" -> {
-                            bancoDeDados.insertRegistro(processadorEmUso, "Uso", 3);
-                            bancoDeDados.insertRegistro(Double.valueOf(dias), "Dias", 3);
-                            bancoDeDados.insertRegistro(Double.valueOf(horas), "Horas", 3);
-                            bancoDeDados.insertRegistro(Double.valueOf(minutos), "Minutos", 3);
-                            bancoDeDados.insertRegistro(Double.valueOf(segundos), "Segundos", 3);
-                            bancoDeDados.insertRegistro(numeroProcessos, "Quantidade", 3);
-                            bancoDeDados.insertRegistro(numeroThreads, "Quantidade", 3);
+                        case 3 -> {
+                            bancoDeDados.insertRegistro(discoDisponivel, "Uso", 3);
+                            bancoDeDados.insertRegistro(swapDisponivel, "SwapDisponivel", 3);
                         }
-                        case "REDE" ->
-                                bancoDeDados.insertRegistro(redeAtual, "Velocidade", 4);
                     }
+
                 }
+                Log log = new Log();
+                log.gravarLog();
             }
         }, delay, interval);
     }
